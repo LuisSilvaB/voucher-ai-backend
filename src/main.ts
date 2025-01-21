5a70cc84a6e2615d56d637a06a9dd4c4a109247b
LuisSilvaB <luisarmandoballadares@gmail.com>
2025-01-21 04:53:39 -0500

    chore: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import * as bodyParser from 'body-parser';
+import { INestApplication } from '@nestjs/common';
+import { Express } from 'express';
+
+let app: INestApplication;
+let expressApp: Express;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get('port');
-  app.use(bodyParser.json({ limit: '10mb' }));
-  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-  app.enableCors({
-    origin: configService.get('corsOrigin') ?? 'http://localhost:4000',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
-  await app.listen(port ?? 3000);
-  console.log(`Server running on port ${port} 🚀`);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
+    app.enableCors({
+      origin: configService.get('corsOrigin') ?? 'http://localhost:4000',
+      methods: 'GET,POST,PUT,DELETE',
+      allowedHeaders: 'Content-Type, Authorization',
+    });
+
+    await app.init();
+    expressApp = app.getHttpAdapter().getInstance();
+  }
+  return expressApp;
 }
-bootstrap();
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then(async () => {
+    const configService = app.get(ConfigService);
+    const port = configService.get('port') || 3000;
+    await app.listen(port);
+    console.log(`Server running on port ${port} 🚀`);
+  });
+}
+
+export default async function handler(req: any, res: any) {
+  const expressApp = await bootstrap();
+  return new Promise((resolve, reject) => {
+    expressApp(req, res, (err?: any) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(undefined);
+    });
+  });
+}
